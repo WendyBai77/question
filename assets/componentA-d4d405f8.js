558e5e0a9cf85413837a95d7698cfeccfe58e14d
wendy <candy1992517@gmail.com>
2023-02-13 18:47:27 +0800

    deploy

--- assets/componentA-d4d405f8.js
+++
@@ -1,1 +0,0 @@
-import{_ as c}from"./index-34cf65be.js";const e={};function n(r,t){return"元件 A"}const _=c(e,[["render",n]]);export{_ as default};
---
+++ assets/componentB-079997b8.js
@@ -0,0 +1,1 @@
+import{_ as c}from"./index-6dfa1b59.js";const e={};function n(r,t){return"元件 B"}const _=c(e,[["render",n]]);export{_ as default};
